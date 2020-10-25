f992677e21f1693877320f66d30068e40d62578e
alex <alex@tekartik.com>
2020-10-25 10:45:14 +0100

    feat: add ArgumentsOption

---
+++ lib/arguments_option.dart
@@ -0,0 +1,19 @@
+import 'location_info_utils.dart';
+
+/// Convenient arguments option
+class ArgumentsOption {
+  final String name;
+
+  ArgumentsOption(this.name);
+
+  /// True if contains, false if "no-xxx" is contains, null otherwier
+  bool /*?*/ has() {
+    if (locationInfo.arguments.containsKey('$name')) {
+      return true;
+    }
+    if (locationInfo.arguments.containsKey('no-$name')) {
+      return false;
+    }
+    return null;
+  }
+}
