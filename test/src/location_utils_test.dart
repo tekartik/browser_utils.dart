507ff75dc0f5ec4568eca0a8b859e25e030eac2d
Alexandre Roux <alex@tekartik.com>
2024-06-05 15:43:00 +0200

    feat: test using dart2wasm

--- test/src/location_utils_test.dart
+++ test/src/location_utils_test.dart
@@ -1,7 +1,5 @@
 @TestOn('browser')
-// Copyright (c) 2016, Alexandre Roux Tekartik. All rights reserved. Use of this source code
-
-// is governed by a BSD-style license that can be found in the LICENSE file.
+library;
 
 import 'package:dev_test/test.dart';
 import 'package:tekartik_browser_utils/location_info_utils.dart';
